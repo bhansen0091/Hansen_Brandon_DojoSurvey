6d04dc185a00878b43d2448d1873bb88eb12b01c
bhansen0091 <bhansen0091@yahoo.com>
2021-02-17 11:23:45 -0600

    assignment update

--- dojo_survey/survey_app/urls.py
+++ dojo_survey/survey_app/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.root),
-    path("survey_complete", views.survey_complete),
     path("result", views.result),
 ]
--- dojo_survey/survey_app/views.py
+++ dojo_survey/survey_app/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import redirect, render
 def root(request):
     return render(request, "index.html")
 
-def survey_complete(request):
+def result(request):
     name_from_form = request.POST['name']
     email_from_form = request.POST['email']
+    radio_btn_from_form = request.POST['radio_btn']
+    drop_down_from_form = request.POST['drop_down_selection']
+    comment_from_form = request.POST['comment_section']
     context = {
         "name_from_template": name_from_form,
-        "email_from_template": email_from_form
+        "email_from_template": email_from_form,
+        "radio_btn_from_template": radio_btn_from_form,
+        "drop_down_selection": drop_down_from_form,
+        "comment_section": comment_from_form
     }
-    return redirect("/result")
-
-def result(request):
-    return render(request, "result.html")
+    return render(request, "result.html", context)
 
 
